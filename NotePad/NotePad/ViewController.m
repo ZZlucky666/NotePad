78412a48261956784dd6178d3f1b8d5a2a0acc1a
JackZeng <dazeng@cengtiandeMacBook-Pro.local>
2016-12-27 17:28:19 +0800

    MOD: 创建工程

---
+++ NotePad/NotePad/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  NotePad
+//
+//  Created by Jack Zeng on 16/12/27.
+//  Copyright © 2016年 Jack Zeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
